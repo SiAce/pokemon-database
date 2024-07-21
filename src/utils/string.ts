c36778a1112372e6f6becb65e367eda41272eed0
SiAce <zd790@nyu.edu>
2024-07-20 23:48:49 -0700

    Add details for pokemons

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -1,3 +1,7 @@
 export function capitalizeFirstLetter(s: String) {
   return s[0].toUpperCase() + s.slice(1)
 }
+
+export function capitalizeSnakeCase(snakeCase: String) {
+  return snakeCase.split("-").map(word => capitalizeFirstLetter(word)).join(" ")
+}
