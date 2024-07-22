b3235e629b290b192ad93e364973e835eb4fe2fd
SiAce <zd790@nyu.edu>
2024-07-22 11:30:55 -0700

    Clean Astro DB configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,33 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 import tailwind from "@astrojs/tailwind";
-
-import db from "@astrojs/db";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [starlight({
-    title: 'Pokémon Database',
-    social: {
-      github: 'https://github.com/withastro/starlight'
-    },
-    sidebar: [{
-      label: 'Pokémon',
-      items: [{
-        label: 'Pokemons',
-        link: '/pokemons/pokemon'
-      }]
-    }],
-    customCss: [
-    // Path to your Tailwind base styles:
-    './src/tailwind.css']
-  }), tailwind({
-    applyBaseStyles: false
-  }), db()]
+  integrations: [
+    starlight({
+      title: "Pokémon Database",
+      social: {
+        github: "https://github.com/withastro/starlight",
+      },
+      sidebar: [
+        {
+          label: "Pokémon",
+          items: [
+            {
+              label: "Pokemons",
+              link: "/pokemons/pokemon",
+            },
+          ],
+        },
+      ],
+      customCss: [
+        // Path to your Tailwind base styles:
+        "./src/tailwind.css",
+      ],
+    }),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
 });
