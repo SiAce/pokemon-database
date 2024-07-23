c59628157d295208bfcf97c5d6831806f86e0165
SiAce <zd790@nyu.edu>
2024-07-23 16:32:58 -0700

    Optimize images

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  image: {
+    domains: ["raw.githubusercontent.com"],
+  },
   integrations: [
     starlight({
       title: "Pokémon Database",
