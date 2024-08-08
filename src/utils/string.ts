ebccc6565cd202f8b31d048c47f3d0245d05241f
SiAce <zd790@nyu.edu>
2024-08-08 00:15:17 -0700

    Smooth type icon and fix version abbreviation

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -17,7 +17,7 @@ export function abbreviation(s: string) {
 
 const IGNORE_WORDS = new Set(["The", "of", "Let's", "Go,"])
 export function versionAbbreviation(version: string) {
-  const spaceSeparated = version.replace(/([a-z])([A-Z])/g, '$1 $2');
+  const spaceSeparated = version.replace(/([a-zA-Z])([A-Z])/g, '$1 $2');
   const letters: string[] = []
   spaceSeparated.split(" ").forEach(word => {
     if (!IGNORE_WORDS.has(word)) {
