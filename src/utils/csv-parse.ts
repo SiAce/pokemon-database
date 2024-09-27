c79dee35ccaf7e0e591b9eeff114fc9acbc5bf92
SiAce <zd790@nyu.edu>
2024-09-26 21:11:50 -0700

    Use pokeapi submodule as data source

--- src/utils/csv-parse.ts
+++ src/utils/csv-parse.ts
@@ -1,8 +1,8 @@
 import { inferSchema, initParser } from "udsv";
 import { readFile } from "node:fs/promises";
 
-export async function parseCsvFile(csvFilePath: string): Promise<any[]> {
-  const csvString = await readFile(csvFilePath, { encoding: "utf8" });
+export async function parseCsvFile(csvFilePath: string, csvBasePath: string = ""): Promise<any[]> {
+  const csvString = await readFile(csvBasePath + csvFilePath, { encoding: "utf8" });
   const schema = inferSchema(csvString);
   const parser = initParser(schema);
   return parser.typedObjs(csvString);
